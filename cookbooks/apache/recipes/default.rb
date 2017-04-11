940a7bbfd780a3aeee223a22e86f80cf95ab9432
chas <redmercrury34@live.com>
2017-04-11 10:51:22 -0400

    added postgresql cookbook

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,14 +4,14 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 # Install http pkg
-if node ['platform_family'] == "rhel"
+if node['platform_family'] == "rhel"
 	package = "httpd"
-elsif node ['platform_family'] == "debian"
+elsif node['platform_family'] == "debian"
 	package = "apache2"
 end
 
 package 'apache2' do
-	package_name 'httpd' 
+	package_name package  
 	action :install 
 end
 
